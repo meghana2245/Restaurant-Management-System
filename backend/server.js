63bbd7a03eb6df56ab1a6dbed28f882d6500d6ce
meghana2245 <meghanakotha245@gmail.com>
2026-06-13 18:44:07 +0530

    Allow Vercel frontend in CORS

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,9 @@ connectDB();
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+    origin: "https://my-platea-app.vercel.app" 
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
